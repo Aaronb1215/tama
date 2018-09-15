884e8810b2dc7cd7ba2c705fbe8612e2b0bbe602
Aaronb1215 <Aaronb1215@outlook.com>
2018-09-15 10:34:56 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,130 @@
+Object = require "classic"
+peachy = require "peachy"
+lume = require "lume"
+Gamestate = require "hump.gamestate"
+Timer = require "hump.timer"
+Camera = require "hump.camera"
+
+require "tama"
+
+home = {}
+hatchery = {}
+
+width = love.graphics.getWidth()
+height = love.graphics.getHeight()
+	
+love.graphics.setDefaultFilter("nearest", "nearest")
+success = love.window.setMode(640, 480)
+
+
+function love.load()
+	egg = love.graphics.newImage("sprites/egg.png")
+	baby = love.graphics.newImage("sprites/baby.png")
+	food = love.graphics.newImage("sprites/food.png")
+	background = love.graphics.newImage("sprites/background.png")
+
+	sfx_hatch = love.audio.newSource("sfx/hatch.wav", "stream")
+	sfx_emerge = love.audio.newSource("sfx/emerge.wav", "stream")
+	sfx_angry = love.audio.newSource("sfx/angry.wav", "stream")
+	sfx_hurt = love.audio.newSource("sfx/hurt.wav", "stream")
+	sfx_newborn = love.audio.newSource("sfx/newborn.wav", "stream")
+	sfx_hatch = love.audio.newSource("sfx/hatch.wav", "stream")
+	sfx_want = love.audio.newSource("sfx/want.wav", "stream")
+	sfx_no = love.audio.newSource("sfx/no.wav", "stream")
+	sfx_no = love.audio.newSource("sfx/no.wav", "stream")
+	sfx_eat = love.audio.newSource("sfx/eat.wav", "stream")
+
+	camera = Camera(width/2, height/2)
+
+	Gamestate.registerEvents()
+	Gamestate.switch(hatchery)
+
+end
+
+function love.update(dt)
+	Timer.update(dt)
+end
+
+function love.draw()
+end
+
+function love.keypressed(key)
+	tama:keypressed(key)
+	if key == "escape" then
+		love.event.quit()
+	end
+end
+
+function home:enter()
+	love.audio.stop()
+	if tama.lifeStage == "egg" and tama.hatched then
+		tama.lifeStage = "baby"
+	end
+	love.graphics.setBackgroundColor(0.6, 0.2, 0.6)
+	-- tama = Tama(width/2, height/2)
+	camera:zoom(1)
+end
+
+function home:update(dt)
+	tama:update(dt)
+end
+
+function home:draw()
+	love.graphics.print("hunger: " .. tama.hunger, 20, height/2 - 20)
+	-- love.graphics.print("sources: " .. love.audio.getSourceCount(), 20, height/2 - 50)
+	love.graphics.print("frame: " .. tama.baby:getFrame(), 20, height/2 - 50)
+	camera:attach()
+
+	love.graphics.setColor(1, 1, 1)
+	love.graphics.draw(background, background:getWidth(), background:getHeight(), 0, 0.5, 0.5)
+	-- for i=0,width/20 do
+	-- 	for j=0, height/20 do
+	-- 		love.graphics.rectangle("line", 20 * j, 20 * i, 5, 5)
+	-- 	end
+	-- end
+	tama:draw()
+	camera:detach()
+end
+
+function home:keypressed(key)
+end
+
+function hatchery:enter()
+	love.graphics.setBackgroundColor(0.6, 0.3, 0.5)
+	tama = Tama(width/2, height/2)
+	camera:zoom(4)
+end
+
+function hatchery:update(dt)
+	if not tama.hatched then
+		tama.egg:setSpeed(lume.lerp(tama.egg:getSpeed(), 10, 0.001))
+	end
+
+	if tama.egg:getSpeed() > 5 then
+		tama.egg:setSpeed(1)
+		tama:hatch()
+		Timer.after(3, function() tama.lifeStage = "baby" Gamestate.switch(home) end)
+	end
+
+	tama:update(dt)
+end
+
+function hatchery:draw()
+	camera:attach()
+
+	love.graphics.setColor(1,1,1)
+	for i=0,width/20 do
+		for j=0, height/20 do
+			love.graphics.circle("line", 20 * j, 20 * i, 2)
+		end
+	end
+	tama:draw()
+	camera:detach()
+end
+
+function hatchery:keypressed(key)
+	if key == "space" and not tama.hatched then
+		tama:hatch()
+		Timer.after(3, function() tama.lifeStage = "baby" Gamestate.switch(home) end)
+	end
+end
---
+++ tama.lua
@@ -0,0 +1,132 @@
+Tama = Object:extend()
+
+function Tama:new(x, y)
+	self.x = x
+	self.y = y
+	self.speed = 300 
+	self.width = 32
+	self.height = 32
+	self.hatched = false
+	self.lifeStage = "egg"
+	self.filter = function() 
+					return "slide" 
+					end
+
+
+	self.egg = peachy.new("sprites/egg.json", egg, "Idle")
+	self.baby = peachy.new("sprites/baby.json", baby, "Yes")
+	self.food = peachy.new("sprites/food.json", food, "cremepuff")
+	self.eating = false
+
+	self.hunger = 10
+	self.time = 0
+end
+
+function Tama:update(dt)
+	local x = 0
+	local y = 0
+
+	destination = {}
+	destination.width = self.width
+	destination.height = self.height
+
+
+	destination.x = self.x + x * (self.speed * dt)
+	destination.y = self.y + y * (self.speed * dt)
+
+	self.egg:update(dt)
+	self.baby:update(dt)
+	if self.eating then
+		self.food:update(dt)
+	end
+
+	if self.time < 500 then
+		self.time = self.time + 1
+	elseif self.time >= 500 then
+		self.time = 0
+		self:tick()
+	end
+end
+
+function Tama:draw()
+	if self.lifeStage == "egg" then
+		self.egg:draw(self.x, self.y, 0, 1, 1, self.egg:getWidth()/2, self.egg:getHeight()/2)
+	elseif self.lifeStage == "baby" then
+		self.baby:draw(self.x, self.y, 0, 1, 1, self.baby:getWidth()/2, self.baby:getHeight()/2)
+	end
+
+	if self.eating then
+		self.food:draw(self.x + 16, self.y + 8, 0, 1, 1, self.food:getWidth()/2, self.food:getHeight()/2)
+	end
+
+	
+end
+
+function Tama:keypressed(key)
+	if key == "space" and self.lifeStage == "baby" and not self.eating then
+		self:eat() --Todo: Add a button in-game for doing this instead.
+	end
+end
+
+function Tama:hatch()
+		self.egg:setTag("Hatch")
+		Timer.after(0.3, function() love.audio.play(sfx_hatch) end)
+		Timer.after(0.6, function() self.egg:setTag("Emerge") love.audio.play(sfx_emerge) end)
+		Timer.after(0.6, function() self.egg:setTag("Wobble") love.audio.play(sfx_newborn) end)
+		self.hatched = true
+end
+
+function Tama:eat()
+	--Todo: Add food type parameter, vary effect based on how healthy.
+	if self.hunger >= 10 then
+		self.baby:setTag("TurnRight")
+		Timer.after(0.6, function() 
+				self.eating = true 
+				self.baby:setTag("Eat") 
+				self.food:setFrame(1) 
+				self.food:play()
+
+				Timer.during(5, function()
+						if self.eating then
+							if self.baby:getFrame() == 4 then
+								love.audio.play(sfx_eat)
+							end
+						end
+					end)
+
+				--[[Todo: Change this to something that doesn't rely
+				on the exact, specific duration listed for each frame
+				in the JSON file. Advance frames manually?]]
+				-- Timer.script(function(wait) 
+				-- 		wait(0.5) 
+				-- 		love.audio.play(sfx_eat) 
+				-- 		wait(0.5) 
+				-- 		love.audio.play(sfx_eat) 
+				-- 		wait(0.5) 
+				-- 		love.audio.play(sfx_eat) 
+				-- 		wait(0.5) 
+				-- 		love.audio.play(sfx_eat) 
+				-- 		wait(0.5) 
+				-- 		love.audio.play(sfx_eat) 
+				-- 		wait(0.5)
+				-- 		love.audio.play(sfx_eat) 
+				-- 		wait(0.5)  
+				-- 		love.audio.play(sfx_eat) 
+				-- 	end)
+			end)
+
+		Timer.after(3.8, function() 
+				self.eating = false
+				self.baby:setTag("Yes")
+				self.hunger = self.hunger - 10
+			end)
+	elseif self.hunger < 10 then
+		self.baby:setTag("No")
+		love.audio.play(sfx_no)
+		Timer.after(0.6, function() self.baby:setTag("Yes") love.audio.stop() end)
+	end
+end
+
+function Tama:tick()
+	self.hunger = self.hunger + 10
+end
